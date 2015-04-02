a1c7e9660da2b29fd959ea07ba85f809f15969eb
31nov <31nov@naver.com>
2015-04-02 16:15:18 +0900

    20-11-07

--- html/start2_20_11.js
+++ html/start2_20_11.js
@@ -2,31 +2,57 @@
 var j = 4;
 var liText ='';
    for(i=0;i<j;i++){
-       liText += '<li><a href="game'+i+'">게임'+i+'</a></li>';
+       if(i % 2 == 0){
+           liText += '<li><a class="blackback" href="game'+i+'">게임'+i+'</a></li>'; 
+       }else{
+           liText += '<li><a class="grayback" href="game'+i+'">게임'+i+'</a></li>';
+       }
    }
 $('#gamelistul').html(liText);
 
 // gamebtn 토글 버튼	
 jQuery(function($){
 	var	$gamebtn = $(".gamebtn");
+	var	$gamelist = $(".gamelist");
+	var	$gamebtnToggle = $(".gamebtnToggle");
+	var	$main = $("#main");
+	var	$lnbbtn = $(".lnbbtndiv");
+	var	$gnbbtn = $(".gnbbtn");
+	var	$footer = $("footer");
 
 	$gamebtn.click(function(){
-		$(".gamelist").toggle("slide");
-        
-        
-		if( $gamebtn.html() == '<img src="gamebtn.png">추천메뉴 닫기') {
-            $gamebtn.html('<img src="gamebtn.png">추천메뉴 열기');
-            $('#main').animate({left: '0'});
-            $('.lnbbtn').animate({left: '0'});
-            $('.gnbbtn').animate({left: '0'});
-            $('footer').animate({left: '32px'});
-		}else{
-            $gamebtn.html('<img src="gamebtn.png">추천메뉴 닫기');
-            $('#main').animate({left: '90px'});
-            $('.lnbbtn').animate({left: '90px'});
-            $('.gnbbtn').animate({left: '90px'});
-            $('footer').animate({left: '90px'});
+		$gamelist.toggle("slide");
+        if($gamebtnToggle.text() == '추천메뉴 열기'){
+            $gamebtnToggle.text('추천메뉴 닫기');
+            $main.animate({left: '90px'});
+            $lnbbtn.animate({left: '90px'});
+            $gnbbtn.animate({left: '118px'});
+            $footer.animate({left: '90px'});
+        }else{
+            $gamebtnToggle.text('추천메뉴 열기');
+            $main.animate({left: '0'});
+            $lnbbtn.animate({left: '0'});
+            $gnbbtn.animate({left: '0'});
+            $footer.animate({left: '32px'});         
+                   
         }
+        
+//        $(".gamelist").toggle("slide");
+//        
+//        
+//		if( $gamebtn.html() == '<img src="gamebtn.png">추천메뉴 닫기') {
+//            $gamebtn.html('<img src="gamebtn.png">추천메뉴 열기');
+//            $('#main').animate({left: '0'});
+//            $('.lnbbtn').animate({left: '0'});
+//            $('.gnbbtn').animate({left: '0'});
+//            $('footer').animate({left: '32px'});
+//		}else{
+//            $gamebtn.html('<img src="gamebtn.png">추천메뉴 닫기');
+//            $('#main').animate({left: '90px'});
+//            $('.lnbbtn').animate({left: '90px'});
+//            $('.gnbbtn').animate({left: '90px'});
+//            $('footer').animate({left: '90px'});
+//        }
 	});	
 });
 
