3afbbd497ef8ac63387dbe427a514fc7eccd8d95
31nov <31nov@naver.com>
2015-06-23 16:50:53 +0900

    2부 추가
    2부-새글 등록 시 이미지 추가 기능 설명 추

--- addimage_css_js/addimage2.js
+++ addimage_css_js/addimage2.js
@@ -16,9 +16,16 @@ jQuery(function($){
     var $nav10 = $('.nav10');
     $('#1').css({display:'inline'}).siblings().css({display:'none'});
     $nav1.css({background:'#000'}).css({color:'#fff'});
-    $('.check1').css({top:375}).css({left:430}).css({width:330}).css({height:270});   
+    $('.check1').css({top:0}).css({left:0}).css({width:0}).css({height:0});   
 
-
+    $('.page5').on('click',function(event){
+        page=5;
+        $nav5.css({background:'#000'}).css({color:'#fff'});
+        $nav5.parent().siblings().children().css({background:'#fff'}).css({color:'#337ab7'});
+        $('#5').css({display:'inline'}).siblings().css({display:'none'});
+        classCheck();
+        checkAnimation();
+    });
     $front.on('click',function(event){
         if(page > 1){
             page -= 1;
@@ -63,28 +70,28 @@ jQuery(function($){
         if($('.check').is('.check1')){
             $('.check1').animate({top:0, left:0, width:0, height:0}, 500);   
         }else if($('.check').is('.check2')){
-            $('.check2').animate({top:305, left:436, width:334, height:24}, 500);   
+            $('.check2').animate({top:569, left:408, width:379, height:47}, 500);   
         }else if($('.check').is('.check3')){
-            $('.check3').animate({top:204, left:253, width:304, height:0}, 500);   
+            $('.check3').animate({top:438, left:118, width:349, height:0}, 500);   
         }else if($('.check').is('.check4')){
-            $('.check4').animate({top:295, left:253, width:314, height:0}, 500);   
+            $('.check4').animate({top:457, left:43, width:745, height:448}, 500);   
         }else if($('.check').is('.check5')){
-            $('.check5').animate({top:197, left:60, width:689, height:83}, 500);   
+            $('.check5').animate({top:444, left:120, width:663, height:75}, 500);   
+            $('.checka').animate({top:858, left:120, width:663, height:61}, 500);  
         }else if($('.check').is('.check6')){
-            $('.check6').animate({top:623, left:72, width:134, height:0}, 500);   
+            $('.check6').animate({top:275, left:98, width:496, height:0}, 500);   
+            $('.checka').animate({top:832, left:94, width:685, height:0}, 500);  
         }else if($('.check').is('.check7')){
-            $('.check7').animate({top:638, left:508, width:200, height:184}, 500);   
+            $('.check7').animate({top:573, left:67, width:228, height:0}, 500);   
+            $('.checka').animate({top:662, left:482, width:135, height:0}, 500);  
         }else if($('.check').is('.check8')){
-            $('.check8').animate({top:564, left:111, width:636, height:129}, 500);   
+            $('.check8').animate({top:825, left:40, width:82, height:0}, 500);  
         }else if($('.check').is('.check9')){
-            $('.check9').animate({top:535, left:376, width:171, height:50}, 500);   
+            $('.check9').animate({top:425, left:95, width:577, height:0}, 500);   
+            $('.checka').animate({top:721, left:73, width:276, height:62}, 500);  
         }else if($('.check').is('.check10')){
-            $('.check10').animate({top:522, left:40, width:659, height:81}, 500);   
-            $('.check11').animate({top:645, left:40, width:729, height:47}, 500);   
-        }else if($('.check').is('.check11')){
-            $('.check11').animate({top:0, left:0, width:0, height:0}, 500);   
-        }
-            
+            $('.check10').animate({top:0, left:0, width:0, height:0}, 500);   
+        } 
     }
     $('.comment').on('click',function(event){
         $('.commentBox').slideToggle(); 
